fad889338a75dfc70d9abedcc5c572ec543d9b47
Joel Bernstein <joelb856@gmail.com>
2026-04-17 18:03:00 -0400

    initial commit w/ barebones databricks bundle, including setup DDL

---
+++ README.md
@@ -0,0 +1,3 @@
+## What are we Singing About? (Databricks edition)
+
+This is a new implementation of [a previous project](joelb856/what-are-we-singing-about) to ingest, process, and serve an analysis of the lyics and metadata associated with the [weekly Billboard Hot 100 songs](https://www.billboard.com/charts/hot-100/) using Databricks Free Edition.
---
+++ src/setup.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE CATALOG IF NOT EXISTS hot100;
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE SCHEMA IF NOT EXISTS hot100.raw;
+# MAGIC CREATE SCHEMA IF NOT EXISTS hot100.processed;
+# MAGIC CREATE SCHEMA IF NOT EXISTS hot100.serving
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE VOLUME IF NOT EXISTS hot100.raw.landing;
